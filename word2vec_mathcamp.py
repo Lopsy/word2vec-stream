7521da1ccf46b87d0515d623768c891652b4d6f8
Lopsy <linyks@gmail.com>
2017-07-12 23:25:59 -0700

    Add files via upload

---
+++ word2vec_mathcamp.py
@@ -0,0 +1,89 @@
+import gzip
+import struct
+
+
+# REPLACE THIS WITH THE FILEPATH FOR YOUR 1.5GB WORD2VEC FILE
+gfile = "C:/Python27/random_files/google_word2vec.gz"
+
+
+
+def word2vec(word, matchCase=True):
+    """ Returns the 300-element vector corresponding to `word`.
+    If you want to vectorize more than one word at once, gvecs is faster. """
+    return gvecs([word], matchCase=True, verbose=0)[0]
+
+
+def gvecs(targetWords, matchCase=True, asDict=False, verbose=1):
+    """ Returns dict {word -> Google representation of word}.
+            matchCase: angel is different from Angel
+            asDict: {word:vec} if true, else [list of vecs] """
+    if not asDict: assert type(targetWords) == list
+    targetWords = list(targetWords)
+    wordsToFind = len(targetWords)
+    # get locations of unfound words:
+    unfound = {word.replace("_"," "):i for i,word in enumerate(targetWords)}
+    if not matchCase: unfound = {w.lower():i for (w,i) in unfound.items()}
+    D = {} if asDict else [None for word in targetWords] # return objects
+    #i=0
+    with gzip.GzipFile(gfile) as f:
+        f.read(1217) # up to right before first "in"
+        while len(unfound) > 0:
+            word = ""
+            while True:
+                c = f.read(1)
+                if c == " ":
+                    break
+                else:
+                    word += c
+            #i += 1
+            #if i%1000==1: print i, word
+            nums = f.read(1200)
+            normalizedWord = word.replace("_"," ")
+            if not matchCase: normalizedWord = normalizedWord.lower()
+            if normalizedWord in unfound:
+                vec = [struct.unpack('>f',
+                    nums[4*i+3:4*i-1:-1] if i>0 else nums[3::-1])[0] \
+                   for i in xrange(300)]
+                if asDict:
+                    D[targetWords[unfound[word]]] = vec
+                else:
+                    D[unfound[word]] = vec
+                del unfound[normalizedWord]
+                if verbose and len(targetWords)>1: print word,
+    return D
+                
+
+
+def gstream():
+    """ Yields pairs (word, vec) from the 300-dimensional Google .gz file.
+    Example use:
+    for (word, vec) in gstream(): [do something]"""
+    with gzip.GzipFile(gfile) as f:
+        f.read(1217) # up to right before first "in"
+        while True:
+            word = ""
+            while True:
+                c = f.read(1)
+                if c == " ":
+                    break
+                else:
+                    word += c
+            nums = f.read(1200)
+            # The next line interprets every 4-byte chunk as a floating point.
+            # See the "value = sign * exp * pre" answer here:
+            # https://stackoverflow.com/questions/27324292
+            vec = [struct.unpack('>f',
+                    nums[4*i+3:4*i-1:-1] if i>0 else nums[3::-1])[0] \
+                   for i in xrange(300)]
+            yield (word, vec)
+    
+def unit(vec):
+    vecnorm = norm(vec)
+    return [x/vecnorm for x in vec]
+def norm(vec):
+    return dot(vec,vec)**0.5
+def dot(v,w): return sum(x*y for x,y in zip(v,w))
+def normalizedDot(v, w):
+    """ cosine of the angle between v and w. """
+    return dot(v,w)/(norm(v)*norm(w))
+    
